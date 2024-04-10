09ad9da6c36d38079e1a5307ef210d49968216e2
2Kronos <rayachrinovic@gmail.com>
2024-04-10 21:45:32 +0200

    test dot

---
+++ main.js
@@ -0,0 +1,75 @@
+const canvas = document.querySelector('canvas');
+const gl = canvas.getContext('webgl');
+
+if (!gl) {
+    throw new Error('WebGL not supported');
+}
+
+// Vertices
+const vertexData = [
+    0.3, 0.4, 0
+];
+
+// Buffer
+const buffer = gl.createBuffer();
+if (!buffer) {
+    console.error("Failed to create buffer");
+} else {
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
+}
+
+// Vertex shader
+const vsSource = `
+    precision mediump float;
+    attribute vec3 pos;
+    void main() {
+        gl_Position = vec4(pos, 1.0);
+        gl_PointSize = 50.0;
+    }
+`;
+const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(vertexShader, vsSource);
+gl.compileShader(vertexShader);
+
+// Error checking for vertex shader
+if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+    console.error(`Vertex shader compilation error: ${gl.getShaderInfoLog(vertexShader)}`);
+}
+
+// Fragment shader
+const fsSource = `
+    precision mediump float;
+    void main() {
+        gl_FragColor = vec4(0.8, 0, 0, 1);
+    }
+`;
+const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource(fragmentShader, fsSource);
+gl.compileShader(fragmentShader);
+
+// Error checking for fragment shader
+if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+    console.error(`Fragment shader compilation error: ${gl.getShaderInfoLog(fragmentShader)}`);
+}
+
+// Program
+const program = gl.createProgram();
+gl.attachShader(program, vertexShader);
+gl.attachShader(program, fragmentShader);
+gl.linkProgram(program);
+
+// Linking error
+if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    console.error(`Shader program linking error: ${gl.getProgramInfoLog(program)}`);
+}
+
+const positionLocation = gl.getAttribLocation(program, "pos");
+gl.enableVertexAttribArray(positionLocation);
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
+
+gl.clearColor(0, 0, 0, 0); // Set clear color
+gl.clear(gl.COLOR_BUFFER_BIT);
+gl.useProgram(program);
+gl.drawArrays(gl.POINTS, 0, 1);
